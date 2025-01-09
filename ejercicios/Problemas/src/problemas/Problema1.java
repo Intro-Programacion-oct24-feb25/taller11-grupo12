d266f8d07447590be61607df9c52febae12c9f51
abrahamx02 <georssj@gmail.com>
2025-01-09 11:42:04 -0500

    problema

--- ejercicios/Problemas/src/problemas/Problema1.java
+++ ejercicios/Problemas/src/problemas/Problema1.java
@@ -15,12 +15,28 @@ public class Problema1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
-        imprimirDatos(informacion);
+        int[][] datos = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        imprimirDatos(datos);
+        
     }
 
     public static void imprimirDatos(int[][] datos) {
+            for (int i = 0 ; i < datos.length ; i++){
+                 for (int j = 0 ; j < datos[i].length ;j++){
+                        if (datos[i][j]% 2 == 0){
+                            
+
+                 }
+                System.out.println (datos[i][j]);
+                    
+                    
+                }
+            }
+        
 
     }
-
+    
+            
+        
+    
 }
