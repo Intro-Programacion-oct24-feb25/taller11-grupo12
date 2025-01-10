efe130038414443c1040c34a019f64da3efd7563
Joeldo04 <joeladominguez04@gmail.com>
2025-01-10 01:32:08 -0500

    G12

--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -5,10 +5,43 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
+
+    public static void main(String[] args) {
+        String[] ciudadesEcuador = arreglo1();
+        procedimientoCiudad(ciudadesEcuador);
+    }
+
+    public static String[] arreglo1() {
+        Scanner entrada = new Scanner(System.in);
+
+        int lim;
+        System.out.println("Ingrese el limite de ciudades");
+        lim = entrada.nextInt();
+        entrada.nextLine();
+
+        String[] ciudades = new String[lim];
+
+        for (int i = 0; i < lim; i++) {
+            System.out.println("Ingrese el nombre de la ciudad " + (i + 1) + ":");
+            ciudades[i] = entrada.nextLine();
+        }
+
+        return ciudades;
+    }
+
+    public static void procedimientoCiudad(String[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if ((arreglo[i].length() == 4) || (arreglo[i].length() == 5)) {
+                System.out.printf("%s\n", arreglo[i]);
+            }
+        }
+    }
+
 }
