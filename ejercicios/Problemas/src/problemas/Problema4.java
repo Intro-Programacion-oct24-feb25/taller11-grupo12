b7933813957472f2c0155e1528461f97c18ba09e
Joeldo04 <joeladominguez04@gmail.com>
2025-01-10 00:34:29 -0500

    G12

--- ejercicios/Problemas/src/problemas/Problema4.java
+++ ejercicios/Problemas/src/problemas/Problema4.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -15,7 +17,68 @@ public class Problema4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        boolean bandera = true;
+        String nombre;
+        String cedula;
+        while (bandera) {
+            System.out.println("Ingrese su nombre completo");
+            nombre = entrada.nextLine();
+            System.out.println("Ingrese su numero de cedula");
+            cedula = entrada.nextLine();
+
+            System.out.println("Que proceso desea realizar\n"
+                    + "1. Calcular valor de la planilla de luz\n"
+                    + "2. Calcular el predio de un bien inmueble");
+            int numero = entrada.nextInt();
+            switch (numero) {
+                case 1:
+                    calcularLuz(nombre, cedula);
+                    break;
+                case 2:
+                    calcularPredio(nombre, cedula);
+                    break;
+            }
+            entrada.nextLine();
+            System.out.println("Ingrese (si) para salir del proceso ");
+            String salida = entrada.nextLine();
+            if (salida.equalsIgnoreCase("si")) {
+                bandera = false;
+            }
+
+        }
+
     }
-    
+
+    public static void calcularLuz(String a, String b) {
+        Scanner entrada = new Scanner(System.in);
+        double kilovatio;
+        double num;
+        double operacion;
+        System.out.println("Ingrese el valor del kilovatio");
+        kilovatio = entrada.nextDouble();
+        System.out.println("Ingrese el numero de kilovatios consumidos");
+        num = entrada.nextDouble();
+        operacion = kilovatio * num;
+        System.out.printf("Cliente %s con cedula %s debe cancelar el "
+                + "valor de $%.2f\n", a, b, operacion);
+
+    }
+
+    public static void calcularPredio(String a, String b) {
+        Scanner entrada = new Scanner(System.in);
+        double inmueble;
+        double operacion;
+
+        System.out.println("Ingrese el valor del inmueble");
+        inmueble = entrada.nextDouble();
+
+        operacion = inmueble * 0.2;
+
+        System.out.printf("Cliente %s con cedula %s tiene un bien inmueble "
+                + "valorado en $%.2f y tiene que pagar de predio $%.2f\n", a, b,
+                inmueble, operacion);
+
+    }
+
 }
