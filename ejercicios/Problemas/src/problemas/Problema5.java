7717dbb193ed77b1ffcb24c13446a28a5c05f911
Joeldo04 <joeladominguez04@gmail.com>
2025-01-10 00:47:57 -0500

    G12

--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -10,5 +10,32 @@ package problemas;
  * @author reroes
  */
 public class Problema5 {
-    
+
+    public static void main(String[] args) {
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+
+        int[][] suma = sumaArreglos(informacion, informacion2);
+
+        for (int i = 0; i < suma.length; i++) {
+            for (int j = 0; j < suma[i].length; j++) {
+                System.out.print(suma[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] sumaArreglos(int[][] a, int[][] b) {
+        int filas = a.length;
+        int columnas = a[0].length;
+        int[][] suma = new int[filas][columnas];
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                suma[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        return suma;
+    }
 }
